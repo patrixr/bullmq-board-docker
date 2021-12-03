e06e44f7ec9297c445bfd093805d1ea55ef2bc0b
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2021-12-02 22:39:24 -0300

    Add auth + eslint

--- README.md
+++ README.md
@@ -11,6 +11,9 @@ docker run -p 3000:3000 -e REDIS_URL=redis://host.docker.internal patrixr/bullmq
 ## Environment variables
 
 - `REDIS_URL` The redis connection string
+- `REDIS_SSL` When the redis use SSL
 - `BULL_PREFIX` Redis key prefix (defaults to bull)
-- `BASE_PATH` The sub url on which to mount the dashboard (defaults to /)
 - `PORT` The port to run on (defaults to 3000)
+- `LOGIN_USER` The userlogin (defaults bull)
+- `LOGIN_PASSWORD` The user password (defaults board)
+- `LOGIN_USERNAME` The username (defaults bull-board)
--- index.js
+++ index.js
@@ -1,44 +1,87 @@
-const { createBullBoard } = require('bull-board')
-const { BullMQAdapter }   = require('bull-board/bullMQAdapter')
-const { Queue }           = require('bullmq')
-const express             = require('express')
-const Redis               = require('ioredis')
-const _                   = require('lodash')
+/* eslint-disable no-console */
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
+if (!process.env.NODE_ENV) {
+  require('dotenv').config();
+}
+
+const { createBullBoard } = require('@bull-board/api');
+const { BullMQAdapter } = require('@bull-board/api/bullMQAdapter');
+const { ExpressAdapter } = require('@bull-board/express');
+const { Queue } = require('bullmq');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+const { ensureLoggedIn } = require('connect-ensure-login');
+const express = require('express');
+const Redis = require('ioredis');
+const _ = require('lodash');
+const passport = require('./passport');
 
 const {
   BULL_PREFIX = 'bull',
-  BASE_PATH = '/',
   PORT = 3000,
   REDIS_URL,
+  REDIS_SSL,
 } = process.env;
 
-(async function() {
-
-  const redis = new Redis(REDIS_URL);
+const start = async () => {
+  const options = REDIS_SSL ? { tls: {} } : {};
+  const redis = new Redis(REDIS_URL, options);
+  const LOGIN_PATH = '/ui/login';
 
   const buildQueues = async () => {
     const keys = await redis.keys(`${BULL_PREFIX}:*`);
     return _.chain(keys)
-    .map(k => k.replace(/^.+?:(.+?):.+?$/, '$1'))
-    .uniq()
-    .map(name => new Queue(name, { connection: redis }))
-    .value();
-  }
-
-  const { router, replaceQueues } = createBullBoard(
-    (await buildQueues()).map(q => new BullMQAdapter(q))
-  )
+      .map((k) => k.replace(/^.+?:(.+?):.+?$/, '$1'))
+      .uniq()
+      .map((name) => new Queue(name, { connection: redis }))
+      .value();
+  };
 
   const app = express();
 
-  app.post('_reload', async (_, res) => {
-    replaceQueues(await buildQueues());
-    res.json({ ok: true });
-  })
+  const serverAdapter = new ExpressAdapter();
+  serverAdapter.setBasePath('/ui');
 
-  app.use(BASE_PATH, router);
+  createBullBoard({
+    queues: (await buildQueues()).map((q) => new BullMQAdapter(q)),
+    serverAdapter,
+  });
+
+  // Configure view engine to render EJS templates.
+  app.set('views', `${__dirname}/views`);
+  app.set('view engine', 'ejs');
+
+  app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true,
+  }));
+
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+  // Initialize Passport and restore authentication state, if any, from the session.
+  app.use(passport.initialize({}));
+  app.use(passport.session({}));
+
+  app.get(LOGIN_PATH, (req, res) => res.render('login'));
+
+  app.post(
+    LOGIN_PATH,
+    passport.authenticate('local', { failureRedirect: LOGIN_PATH }),
+    (req, res) => res.redirect('/ui'),
+  );
+
+  app.use(
+    '/ui',
+    ensureLoggedIn({ redirectTo: LOGIN_PATH }),
+    serverAdapter.getRouter(),
+  );
 
   app.listen(PORT, () => {
-    console.log(`bull-board listening on host:${PORT}${BASE_PATH}`);
+    console.log(`bull-board listening on host:${PORT}`);
   });
-})();
+};
+
+start().catch((e) => console.error(e));
